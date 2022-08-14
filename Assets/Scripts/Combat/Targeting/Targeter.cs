58a098e14db5070797cf6ca148a9db5815c36a5c
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-14 16:06:39 -0500

    Made targeting dynamic

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -1,27 +1,41 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Targeter : MonoBehaviour
 {
+    [SerializeField] CinemachineTargetGroup targetGroup;
+
     List<Target> targets = new List<Target>();
 
     public Target CurrentTarget { get; private set; }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<Target>(out Target target))
-        {
-            targets.Add(target);
-        }
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
+        targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.TryGetComponent<Target>(out Target target))
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
+        RemoveTarget(target);
+    }
+
+    void RemoveTarget(Target target)
+    {
+        if (CurrentTarget == target)
         {
-            targets.Remove(target);
+            targetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
         }
+
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
     }
 
     public bool SelectTarget()
@@ -29,11 +43,15 @@ public class Targeter : MonoBehaviour
         if (targets.Count == 0) { return false; }
 
         CurrentTarget = targets[0];
+        targetGroup.AddMember(CurrentTarget.transform, 1, 2);
         return true;
     }
 
     public void Cancel()
     {
+        if (CurrentTarget == null) { return; }
+
+        targetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
     }
 }
