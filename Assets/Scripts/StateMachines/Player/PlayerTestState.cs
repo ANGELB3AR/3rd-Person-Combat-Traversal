41b7c7eab62a8e8b3d5c64682e3d8f39d0f1f5de
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-14 05:46:46 -0500

    Created character controlled and set up initial movement

--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -18,7 +18,10 @@ public class PlayerTestState : PlayerBaseState
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y;
 
-        stateMachine.transform.Translate(movement * deltaTime);
+        stateMachine.Controller.Move(movement * deltaTime * stateMachine.FreeLookMovementSpeed);
+
+        if (stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
 
     public override void Exit()
