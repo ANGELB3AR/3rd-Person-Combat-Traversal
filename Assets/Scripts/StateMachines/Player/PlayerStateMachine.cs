18cadab8cc46f3492f6597773e634d45f4c32e49
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-14 07:48:47 -0500

    Changed movement vector to be based on camera instead of player model

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -9,8 +9,12 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public Animator Animator { get; private set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
+    public Transform MainCameraTransform { get; private set; }
+
     void Start()
     {
-        SwitchState(new PlayerTestState(this));
+        MainCameraTransform = Camera.main.transform;
+
+        SwitchState(new PlayerFreeLookState(this));
     }
 }
