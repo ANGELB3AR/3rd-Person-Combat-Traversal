56d4f07f4f74fbbdbf684f8565a74812d0f748a6
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-16 06:28:51 -0500

    Created array of attacks

--- Assets/Scripts/StateMachines/Player/Attack.cs
+++ Assets/Scripts/StateMachines/Player/Attack.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Attack
 {
-
+    [field: SerializeField] public string AnimationName { get; private set; }
 }
