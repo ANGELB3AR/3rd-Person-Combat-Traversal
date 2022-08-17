4e20088d5803bacf3dab43a8bda5e0378ee9aff1
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-17 05:30:45 -0500

    Created health script

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 100;
+
+    int health;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void DealDamage(int damage)
+    {
+        if (health <= 0) { return; }
+
+        health -= damage;
+    }
+}
