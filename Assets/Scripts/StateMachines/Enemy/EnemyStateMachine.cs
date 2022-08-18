aced9084c4c92421f6948fba2339b750bf8334b1
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-18 06:02:25 -0500

    Added property for movement speed Disabled nav mesh agent from updating enemy position/rotation

--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -9,6 +9,7 @@ public class EnemyStateMachine : StateMachine
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
     [field: SerializeField] public NavMeshAgent NavMeshAgent { get; private set; }
+    [field: SerializeField] public float MovementSpeed { get; private set; }
     [field: SerializeField] public float PlayerChaseRange { get; private set; }
     
     public GameObject Player { get; private set; }
@@ -16,6 +17,9 @@ public class EnemyStateMachine : StateMachine
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+
+        NavMeshAgent.updatePosition = false;
+        NavMeshAgent.updateRotation = false;
 
         SwitchState(new EnemyIdleState(this));
     }
