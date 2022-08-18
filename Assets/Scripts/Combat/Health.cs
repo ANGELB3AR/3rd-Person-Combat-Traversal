2a8c3603609e5a2c308f18c0c7108050b1bddf35
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-18 12:10:30 -0500

    Created event for when damage taken Created transitions to impact states

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int maxHealth = 100;
+
+    public event Action OnTakeDamage;
 
     int health;
 
@@ -18,6 +21,8 @@ public class Health : MonoBehaviour
         if (health == 0) { return; }
 
         health = Mathf.Max(health - damage, 0);
+
+        OnTakeDamage?.Invoke();
 
         Debug.Log(health);
     }
--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -10,6 +10,7 @@ public class EnemyStateMachine : StateMachine
     [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
     [field: SerializeField] public NavMeshAgent Agent { get; private set; }
     [field: SerializeField] public WeaponDamage Weapon { get; private set; }
+    [field: SerializeField] public Health Health { get; private set; }
     [field: SerializeField] public float MovementSpeed { get; private set; }
     [field: SerializeField] public float PlayerChaseRange { get; private set; }
     [field: SerializeField] public float AttackRange { get; private set; }
@@ -26,6 +27,21 @@ public class EnemyStateMachine : StateMachine
         Agent.updateRotation = false;
 
         SwitchState(new EnemyIdleState(this));
+    }
+
+    void OnEnable()
+    {
+        Health.OnTakeDamage += HandleTakeDamage;
+    }
+
+    void OnDisable()
+    {
+        Health.OnTakeDamage -= HandleTakeDamage;
+    }
+
+    void HandleTakeDamage()
+    {
+        SwitchState(new EnemyImpactState(this));
     }
 
     void OnDrawGizmosSelected()
