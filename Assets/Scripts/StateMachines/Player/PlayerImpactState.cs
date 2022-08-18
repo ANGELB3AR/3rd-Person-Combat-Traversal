8350ea385eadd505dcb7f82de81f5c8ed622eaa7
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-18 11:57:23 -0500

    Created impact states for both player & enemy

---
+++ Assets/Scripts/StateMachines/Player/PlayerImpactState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerImpactState : PlayerBaseState
+{
+    readonly int impactHash = Animator.StringToHash("Impact");
+
+    const float crossFadeDuration = 0.1f;
+
+    public PlayerImpactState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(impactHash, crossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit() { }
+}
