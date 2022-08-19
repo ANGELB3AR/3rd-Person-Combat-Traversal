ed89bd6376d10d83c3f323c3661fe5040c28c208
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-19 05:35:56 -0500

    Set up dead states

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyDeadState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeadState : EnemyBaseState
+{
+    public EnemyDeadState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Tick(float deltaTime)
+    {
+
+    }
+
+    public override void Exit() { }
+}
