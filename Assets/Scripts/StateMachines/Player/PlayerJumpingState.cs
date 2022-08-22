9305cfd548dc9a4544f4bf2ccf81345cd2a0bffc
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-08-22 07:23:39 -0500

    Created player jump/fall states

---
+++ Assets/Scripts/StateMachines/Player/PlayerJumpingState.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpingState : PlayerBaseState
+{
+    public PlayerJumpingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit() { }
+}
